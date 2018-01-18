a0e3316d524e9133e6550742a3706104f353e5a7
aekirgil <ahmetemrekirgil@gmail.com>
2018-01-19 00:34:16 +0300

    WebPack configurations made for react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { AppContainer } from 'react-hot-loader';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import App from './components/App';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -17,4 +17,4 @@ const render = Component =>
 render(App);
 
 // Webpack Hot Module Replacement Api
-if (module.hot) module.hot.accept('../components/App', () => render(App) );
+if (module.hot) module.hot.accept('./components/App', () => render(App) );
